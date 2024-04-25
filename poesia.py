756ce2e6676254293c1feb5f5863a0ff9ed0d367
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-25 10:48:23 -0300

    modificado arquivo poesia.py

--- poesia.py
+++ poesia.py
@@ -1,13 +1,25 @@
-print("Antes, e com tal zelo, e sempre, e tanto......")
+print("De tudo, ao meu amor serei atento")
+print("Antes, e com tal zelo, e sempre, e tanto,")
 print("Que mesmo em face do maior encanto")
-print("Dele se encante mais meu pensamento!")
+print("Dele se encante mais meu pensamento.")
 print("\n\n\n")
 
-print("Quero vivê-lo em cada vão momento.")
-print("E em seu louvor hei de espalhar meu canto.")
-print("E rir meu riso e derramar meu pranto.")
-print("Ao seu pesar ou seu contentamento")
+print("Quero vivê-lo em cada vão momento")
+print("E em seu louvor hei de espalhar meu canto")
+print("E rir meu riso e derramar meu pranto")
+print("Ao seu pesar ou seu contentamento.")
 print("\n\n\n")
+
+print("E assim quando mais tarde me procure")
+print("Quem sabe a morte, angústia de quem vive,")
+print("Quem sabe a solidão, fim de quem ama")
+print("\n\n\n")
+
+print("Eu possa me dizer do amor (que tive):")
+print("Que não seja eterno, posto que é chama,")
+print("Mas que seja infinito enquanto dure.")
 
 #modificação teste git 2
 #teste 3
+
+#mudança no arquivo depois de baixado do github
